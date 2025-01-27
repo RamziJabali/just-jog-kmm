883f4c4ce00d343c7f31b067c3e68816b8843205
Ramzi Eljabali <ramzi@eljabali.org>
2025-01-26 22:46:14 -0700

    Added preview for scaffold, added navigation bar padding to allow for distinction between gesture bar and bottom bar.

--- shared/src/commonMain/kotlin/ramzi/eljabali/justjogkmm/ui/components/JustJogScaffold.kt
+++ shared/src/commonMain/kotlin/ramzi/eljabali/justjogkmm/ui/components/JustJogScaffold.kt
@@ -1,17 +1,24 @@
 package ramzi.eljabali.justjogkmm.ui.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun JustJogScaffold() {
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        bottomBar = { JustJogBottomNavigation() },
-        floatingActionButton = { /* future requirement */ }
+    Box(
+        Modifier.navigationBarsPadding()
     ) {
-
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            bottomBar = { JustJogBottomNavigation() },
+            floatingActionButton = { /* future requirement */ }
+        ) { padding ->
+            /* Content */gi
+        }
     }
 }
--- shared/src/iosMain/kotlin/ramzi/eljabali/justjogkmm/App.kt
+++ shared/src/iosMain/kotlin/ramzi/eljabali/justjogkmm/App.kt
@@ -2,7 +2,8 @@ package ramzi.eljabali.justjogkmm
 
 import androidx.compose.ui.window.ComposeUIViewController
 import ramzi.eljabali.justjogkmm.ui.GreetingView
+import ramzi.eljabali.justjogkmm.ui.components.JustJogScaffold
 
 fun MainViewController() = ComposeUIViewController {
-    GreetingView(Greeting().greet())
+    JustJogScaffold()
 }
